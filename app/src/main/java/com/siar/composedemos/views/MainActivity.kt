4decf17c598dc64e07957dfd5da3f18df63db872
Pablo Quiroga <jtdoe78@gmail.com>
2023-10-19 08:23:39 -0300

    Basics 1 - refactor architecture of views

--- app/src/main/java/com/siar/composedemos/views/MainActivity.kt
+++ app/src/main/java/com/siar/composedemos/views/MainActivity.kt
@@ -1,30 +1,14 @@
 package com.siar.composedemos.views
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.siar.composedemos.R
 import com.siar.composedemos.ui.theme.ComposeDemosTheme
+import com.siar.composedemos.views.components.MessageCard
+import com.siar.composedemos.views.data.Message
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,67 +21,6 @@ class MainActivity : ComponentActivity() {
                     MessageCard(Message("Android", "Jetpack Compose"))
                 }
             }
-        }
-    }
-}
-
-data class Message(val author: String, val body: String)
-
-@Composable
-fun MessageCard(msg: Message) {
-    Row(
-        modifier = Modifier.padding(all = 8.dp)
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.profile_picture),
-            contentDescription = "Contact profile picture",
-            modifier = Modifier
-                .size(40.dp)
-                .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
-        )
-        Spacer(modifier = Modifier.width(8.dp))
-        Column {
-            Text(
-                text = msg.author,
-                color = MaterialTheme.colorScheme.primary,
-                style = MaterialTheme.typography.bodyLarge
-            )
-            Spacer(modifier = Modifier.width(4.dp))
-            Surface(
-                shape = MaterialTheme.shapes.medium,
-                tonalElevation = 1.dp
-            ) {
-                Text(
-                    text = msg.body,
-                    modifier = Modifier.padding(all = 4.dp),
-                    style = MaterialTheme.typography.bodyMedium
-                )
-            }
-        }
-    }
-
-}
-
-@Preview(
-    name = "Light mode",
-    showBackground = true
-)
-@Preview(
-    name = "Dark mode",
-    showBackground = true,
-    uiMode = UI_MODE_NIGHT_YES
-)
-@Composable
-fun PreviewMessageCard() {
-    ComposeDemosTheme {
-        Surface{
-            MessageCard(
-                msg = Message(
-                    "Colleague",
-                    "Hey, take a look at Jetpack Compose"
-                )
-            )
         }
     }
 }
---
+++ app/src/main/java/com/siar/composedemos/views/data/Message.kt
@@ -0,0 +1,3 @@
+package com.siar.composedemos.views.data
+
+data class Message(val author: String, val body: String)
