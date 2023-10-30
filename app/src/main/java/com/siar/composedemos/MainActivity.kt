dbf5c92dacc3b154265a463e28de4779b96026a6
Pablo Quiroga <jtdoe78@gmail.com>
2023-10-30 09:24:22 -0300

    Refactor for simple architecture - first commit

--- app/src/main/java/com/siar/composedemos/MainActivity.kt
+++ app/src/main/java/com/siar/composedemos/MainActivity.kt
@@ -6,18 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.siar.composedemos.ui.theme.ComposeDemosTheme
+import com.siar.composedemos.views.ui.Greeting
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeDemosTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -26,21 +23,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeDemosTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/siar/composedemos/views/ui/Greeting.kt
@@ -0,0 +1,24 @@
+package com.siar.composedemos.views.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.siar.composedemos.ui.theme.ComposeDemosTheme
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeDemosTheme {
+        Greeting("Android")
+    }
+}
